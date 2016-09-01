fe94732ff03f8f056fdc5a90ff0ebdc7f6f06256
Juha Vierinen <jvierine@gmail.com>
2016-09-01 13:06:26 +0000

    asd

--- sat_predict.py
+++ sat_predict.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
-
+#
+# Juha Vierinen, 2016
+#
+# - plan a schedule for tracking targets, plot the schedule
+# - identify conflicts (overlapping tracks) and resolve them
+# - write an elan file with appropriate commands (AT, pointdir)
+#
+# 
 import ephem, sys, math
 import os, errno, time 
 import matplotlib.pyplot as plt
 import numpy as n
 import sat_elements as se
 from optparse import OptionParser
+import plan_pass as pp
+
+# todo
+# calculate tracklet length in degrees.
+
+# output elan file
+
+# automatic fetching of TLE from space-track.org
 
 def get_track(target,
               startd = ephem.Date("2016/09/01 00:00:00"),
@@ -94,28 +109,48 @@ parser.add_option("-b", "--lon", dest="lon", default="19:13:38.0",
 parser.add_option("-p", "--n_pos", dest="n_pos", default=4,
                   help="Number of pointing positions.")
 
+parser.add_option("-t", "--targets", dest="targets", default="27386,23560,21574,26958,39162,38086",
+                  help="Targets to track, comma separated list of NORAD catalog numbers")
+
 (o, args) = parser.parse_args()
 
 # table t0, t1, dt, peakel, at
 pass_t0=[]
 pass_names=[]
-
+lat=o.lat#print(o.lat)
+lon=o.lon
 
 targets=se.targets
+tracklets=[]
+t0s=[]
+t1s=[]
 results=[]
 for t in targets:
+    # get all passes for target
     t=get_track(target=t,startd=ephem.Date(o.t0),endd=ephem.Date(o.t1))
     for i in range(len(t["p0"])):
         pass_t0.append(t["p0"][i])
         print("%s - %s duration %1.2f min peak_el %1.2f" %(t["p0"][i],t["p1"][i],(t["p1"][i]-t["p0"][i])/ephem.minute,t["peak_el"][i]))
+        tl=pp.plan(t["name"], t["p0"][i], t["p1"][i], lat, lon, 2)
+        tracklets.append(tl)
+        t0s.append(tl["t0"])
+        t1s.append(tl["t1"])
+t0s=n.array(t0s)
+t1s=n.array(t1s)
+idx=n.argsort(t0s)
+for i in idx:
+    print("%s t0 %s t1 %s"%(tracklets[i]["name"],ephem.Date(t0s[i]),ephem.Date(t1s[i])))
+    for ti in range(len(tracklets[i]["track_points"])):
+        print(tracklets[i]["track_points"][ti])
 
-    plt.plot(t["hv"],t["el"],label=t["name"])
 
-plt.xlabel("Time (UTC)")
-plt.ylabel("Elevation (deg)")
-plt.legend()#prop={'size':6})
-plt.axhline(30)
-plt.ylim([0,90])
+#    plt.plot(t["hv"],t["el"],label=t["name"])
 
-plt.show()
+#plt.xlabel("Time (UTC)")
+#plt.ylabel("Elevation (deg)")
+#plt.legend()#prop={'size':6})
+#plt.axhline(30)
+#plt.ylim([0,90])
+
+#plt.show()
 
